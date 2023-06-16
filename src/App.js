96973897f14d68fe9eeeb3c05d348746d0b5e83d
Ricky <rzh4321@gmail.com>
2023-06-16 15:38:37 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      first,
+    };
+  }
+}
+
+export default App;
---
+++ src/components/Paper.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Paper extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      first,
+    };
+  }
+}
+
+export default Paper;
